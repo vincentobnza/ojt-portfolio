b624cd083e8a06a381e85459cc17bec3d4566482
Vincent Obenza <vincentobenza2003@gmail.com>
2025-05-06 20:23:52 +0800

    Added some changes

--- src/pages/showcase.tsx
+++ src/pages/showcase.tsx
@@ -329,7 +329,7 @@ const Card = ({
     <div className="bg-white dark:bg-zinc-800/10 rounded-lg p-3 flex items-start justify-between border border-zinc-200 dark:border-zinc-700/40 relative">
       <div className="flex flex-col gap-4">
         <div className="self-start p-2 rounded-lg bg-zinc-50  dark:bg-zinc-800/30">
-          <Icon className={`text-4xl ${iconColor}`} />
+          <Icon className={`text-2xl ${iconColor}`} />
         </div>
         <h2 className="text-sm font-semibold text-zinc-800 dark:text-zinc-200">
           {title}
